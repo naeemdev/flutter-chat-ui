9704c8afd07bec491600bb503b1c1b656e23c530
Muhammad Naeem <naeem0313@gmail.com>
2020-06-04 09:43:04 +0500

    add dummy messages

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -22,9 +22,9 @@ final User currentUser = User(
   imageUrl: 'assets/images/naeem.png',
 );
 
-final User user1 = User(
+final User zohaib = User(
   id: 1,
-  name: 'Ali',
+  name: 'Zohaib',
   imageUrl: 'assets/images/user.png',
 );
 final User miah = User(
@@ -57,4 +57,112 @@ final User khan = User(
   name: 'Khan',
   imageUrl: 'assets/images/user5.png',
 );
- 
+
+// FAVORITE CONTACTS
+List<User> favorites = [naeem, zohaib, miah, rajaib, khan];
+
+// EXAMPLE CHATS ON HOME SCREEN
+List<Message> chats = [
+  Message(
+    sender: naeem,
+    time: '07:30 PM',
+    text: 'Hi, how\'s how are ?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: miah,
+    time: '08:30 PM',
+    text: 'how was your day',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: rajaib,
+    time: '3:30 PM',
+    text: 'Hello',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: ali,
+    time: '2:30 PM',
+    text: 'Hi',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: khan,
+    time: '3:30 PM',
+    text: 'Hey it\'s testing ',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: asad,
+    time: '12:30 AM',
+    text: 'My first message',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: zohaib,
+    time: '06:30 AM',
+    text: 'Good Morning',
+    isLiked: false,
+    unread: false,
+  ),
+];
+
+// EXAMPLE MESSAGES IN CHAT SCREEN
+List<Message> messages = [
+  Message(
+    sender: zohaib,
+    time: '06:30 AM',
+    text: 'Good Morning',
+    isLiked: true,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '06:30 AM',
+    text: 'Morning how are you today',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: zohaib,
+    time: '06:31 AM',
+    text: 'i am fine thanks',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: zohaib,
+    time: '06:35 AM',
+    text: 'What about you',
+    isLiked: true,
+    unread: false,
+  ),
+  Message(
+    sender: currentUser,
+    time: '7:30 AM',
+    text: 'that\'s Greate ... you are welcome...i am fine too',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: zohaib,
+    time: '1:00 PM',
+    text: 'do you have any plan for  lunch ?.',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '1:10 PM',
+    text: 'don\'t have any ',
+    isLiked: false,
+    unread: true,
+  ),
+];
